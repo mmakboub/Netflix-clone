7d0a94b1fe49985179982832a796c463c1798a5a
Mariam Makboub <mmakboub@e1r7p13.1337.ma>
2024-03-26 21:08:55 +0000

    fixing sign up and sign in

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Header from './components/Header';
 import HomeBanner from './components/HomeBanner';
 import Banner from './components/banner';
 import Login from './components/login';
-import List from './components/list';
+// import List from './components/list';
 function App() {
   return (
     <React.Fragment>
       <Router>
         <Routes>
           <Route path="/" element={<React.Fragment> <Header /> <HomeBanner /> </React.Fragment>} />
-          <Route path="/login" element={<React.Fragment> <Login /> </React.Fragment>} />
-          <Route path="/Dashboard" element={<React.Fragment> <Header /> <Banner />  <List />   </React.Fragment>} />
+          <Route path="/login" element={<React.Fragment> <Header /> <Login /> </React.Fragment>} />
+          <Route path="/register" element={<React.Fragment> <Header /> <Login />     </React.Fragment>} />
+          <Route path="/dashboard" element={<React.Fragment> <Header /> <Banner />     </React.Fragment>} />
         </Routes>
       </Router>
     </React.Fragment>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCvPMyKN9puxucnE_Vi1lNuy_Rgzrq-oz4",
+    authDomain: "netflix-eeac2.firebaseapp.com",
+    projectId: "netflix-eeac2",
+    storageBucket: "netflix-eeac2.appspot.com",
+    messagingSenderId: "459385441803",
+    appId: "1:459385441803:web:524b101c56eb2cb06a810e",
+    measurementId: "G-JM8RTY3N1Y"
+};
